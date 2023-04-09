17ed15c4683702c934f72c294bea542a5833053a
Robin Blondin <robin.blondin@protonmail.com>
2023-04-09 20:32:24 +0200

    Add function for Create account and back button
    Added a functions that let you navigate between the signIn activity and the
    signUp activity.

--- app/src/main/java/com/example/chatapp/activities/SignInActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.kt
@@ -3,10 +3,23 @@ package com.example.chatapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatapp.R
+import com.example.chatapp.databinding.ActivitySignInBinding
+import android.content.Intent
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners()
     }
+    private fun setListeners() {
+        binding.textCreateNewAccount.setOnClickListener {
+            startActivity(Intent(applicationContext, SignUpActivity::class.java))
+        }
+    }
+
 }
--- app/src/main/java/com/example/chatapp/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/SignUpActivity.kt
@@ -3,10 +3,20 @@ package com.example.chatapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatapp.R
+import com.example.chatapp.databinding.ActivitySignInBinding
+import com.example.chatapp.databinding.ActivitySignUpBinding
+import android.content.Intent
 
+private lateinit var binding: ActivitySignUpBinding
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setListeners();
+    }
+
+    private fun setListeners() {
+        binding.textSignIn.setOnClickListener { onBackPressed() }
     }
 }
