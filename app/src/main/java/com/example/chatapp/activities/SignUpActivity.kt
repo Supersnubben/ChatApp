2b1b0d4a6bd070fa408656dd5cbd8635d10bc529
Robin Blondin <robin.blondin@protonmail.com>
2023-04-10 23:02:39 +0200

    Added new functions for the SignUpActivity
    Added functions that lets pick a profile picture from their units
    hard drive, register an account and saves the info at the Firebase
    Firestore.
    
    Added two new classes, Constants and PreferenceManager

--- app/src/main/java/com/example/chatapp/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/SignUpActivity.kt
@@ -1,18 +1,31 @@
 package com.example.chatapp.activities
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.chatapp.R
 import com.example.chatapp.databinding.ActivitySignInBinding
 import com.example.chatapp.databinding.ActivitySignUpBinding
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.provider.MediaStore
+import android.util.Base64
 import android.util.Patterns
+import android.view.View
 import android.widget.Toast
 import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.chatapp.utilities.Constants
+import com.example.chatapp.utilities.PreferenceManager
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import java.io.ByteArrayOutputStream
+import java.io.FileNotFoundException
 
 @SuppressLint("StaticFieldLeak")
 private lateinit var binding: ActivitySignUpBinding
+private var preferenceManager = PreferenceManager(input)
 private lateinit var encodedImage: String
 @Suppress("DEPRECATION")
 class SignUpActivity : AppCompatActivity() {
@@ -20,6 +33,7 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        preferenceManager = PreferenceManager(applicationContext)
         setListeners()
     }
 
@@ -30,18 +44,75 @@ class SignUpActivity : AppCompatActivity() {
                 signUp()
             }
         }
+        binding.layoutImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            pickImage.launch(intent)
+        }
     }
 
     private fun showToast(message: String) {
         Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
     }
-
     private fun signUp() {
+        loading(true)
+        val database = FirebaseFirestore.getInstance()
+        val user = hashMapOf<String, Any>(
+            Constants.KEY_NAME to binding.inputName.text.toString(),
+            Constants.KEY_EMAIL to binding.inputEmail.text.toString(),
+            Constants.KEY_PASSWORD to binding.inputPassword.text.toString(),
+            Constants.KEY_IMAGE to encodedImage
+        )
+        database.collection(Constants.KEY_COLLECTION_USERS)
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                loading(false)
+                preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true)
+                preferenceManager.putString(Constants.KEY_USER_ID, documentReference.id)
+                preferenceManager.putString(Constants.KEY_NAME, binding.inputName.text.toString())
+                preferenceManager.putString(Constants.KEY_IMAGE, encodedImage)
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or  Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                startActivity(intent)
+            }.addOnFailureListener { exception ->
+                loading(false)
+                exception.message?.let { showToast(it) }
+            }
+        }
 
+
+    private fun encodeImage(bitmap: Bitmap): String {
+        val previewWidth = 150
+        val previewHeight = bitmap.height * previewWidth / bitmap.width
+        val previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false)
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val bytes = byteArrayOutputStream.toByteArray()
+        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream)
+
+        return Base64.encodeToString(bytes, Base64.DEFAULT)
+
+        }
+
+    private val pickImage = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            result.data?.data?.let { imageUri ->
+                try {
+                    applicationContext.contentResolver.openInputStream(imageUri)?.use { inputStream ->
+                        val bitmap = BitmapFactory.decodeStream(inputStream)
+                        binding.imageProfile.setImageBitmap(bitmap)
+                        binding.textAddImage.visibility = View.GONE
+                        encodedImage = encodeImage(bitmap)
+                    }
+                } catch (e: FileNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 
     private fun isValidSignUpDetails(): Boolean {
-        if(encodedImage == null) {
+        if(encodedImage.isEmpty()) {
             showToast("Select profile image")
             return false
         }
@@ -73,4 +144,19 @@ class SignUpActivity : AppCompatActivity() {
             return true
         }
     }
+
+    private fun loading (isLoading: Boolean) {
+        if (isLoading) {
+            binding.buttonSignUp.visibility = View.INVISIBLE
+            binding.progressBar.visibility = View.VISIBLE
+        }
+        else {
+            binding.progressBar.visibility = View.INVISIBLE
+            binding.progressBar.visibility = View.VISIBLE
+        }
+    }
+
+
 }
+
+
---
+++ app/src/main/java/com/example/chatapp/utilities/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.chatapp.utilities
+
+class Constants {
+    var KEY_COLLECTION_USERS = "users"
+    var KEY_NAME = "name"
+    var KEY_EMAIL = "email"
+    var KEY_PASSWORD = "password"
+    var KEY_PREFERENCE_NAME = "chatAppPreference"
+    var KEY_IS_SIGNED_IN = "isSignedIn"
+    var KEY_USER_ID = "userId"
+    var KEY_IMAGE = "image"
+}
