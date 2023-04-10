8e4f3683fecad8f1cf3f4e2a44de17354112aa20
Robin Blondin <robin.blondin@protonmail.com>
2023-04-10 20:05:45 +0200

    Created a function to check empty fields.
    Added a function in SignUpActivity that checks the textfield if they
    are empty when you click on the sign up button.

--- app/src/main/java/com/example/chatapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.chatapp
+package com.example.chatapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.chatapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/chatapp/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/SignUpActivity.kt
@@ -7,10 +7,13 @@ import com.example.chatapp.R
 import com.example.chatapp.databinding.ActivitySignInBinding
 import com.example.chatapp.databinding.ActivitySignUpBinding
 import android.content.Intent
+import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.OnBackPressedDispatcher
 
 @SuppressLint("StaticFieldLeak")
 private lateinit var binding: ActivitySignUpBinding
+private lateinit var encodedImage: String
 @Suppress("DEPRECATION")
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +25,52 @@ class SignUpActivity : AppCompatActivity() {
 
     private fun setListeners() {
         binding.textSignIn.setOnClickListener { onBackPressed() }
+        binding.buttonSignUp.setOnClickListener {
+            if (isValidSignUpDetails()) {
+                signUp()
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun signUp() {
+
+    }
+
+    private fun isValidSignUpDetails(): Boolean {
+        if(encodedImage == null) {
+            showToast("Select profile image")
+            return false
+        }
+        else if (binding.inputName.text.toString().trim().isEmpty()) {
+            showToast("Enter name")
+            return false
+        }
+        else if (binding.inputEmail.text.toString().trim().isEmpty()) {
+            showToast("Enter email")
+            return false
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.text.toString()).matches()) {
+            showToast("Enter valid image")
+            return false
+        }
+        else if (binding.inputPassword.text.toString().trim().isEmpty()) {
+            showToast("Enter password")
+            return false
+        }
+        else if (binding.inputConfirmPassword.text.toString().trim().isEmpty()) {
+            showToast("Confirm your password")
+            return false
+        }
+        else if (binding.inputPassword.text.toString() != binding.inputConfirmPassword.text.toString()) {
+            showToast("Password must be the same in both fields")
+            return false
+        }
+        else {
+            return true
+        }
     }
 }
