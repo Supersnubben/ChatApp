ddeb0bc37469aa784a33874c80ac7ca2c3fe257f
Robin Blondin <robin.blondin@protonmail.com>
2023-04-16 22:12:18 +0200

    Added a function that loads user details and inserts them in elements of the activity main layout

--- app/src/main/java/com/example/chatapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.chatapp.activities
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatapp.R
+import com.example.chatapp.databinding.ActivityMainBinding
+import com.example.chatapp.utilities.Constants
+import com.example.chatapp.utilities.PreferenceManager
+import android.util.Base64
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var preferenceManager: PreferenceManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        preferenceManager = PreferenceManager(applicationContext)
+        loadUserDetails()
+    }
+
+    private fun loadUserDetails() {
+        binding.textName.text = preferenceManager.getString(Constants.KEY_NAME)
+        var bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT)
+        var bitmap = BitmapFactory.decodeByteArray(bytes,0, bytes.size)
+        binding.imageProfile.setImageBitmap(bitmap)
     }
 }
