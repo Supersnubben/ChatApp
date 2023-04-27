11f417ee974e9a84e8d4f40d660c5ec68d50630f
Robin Blondin <robin.blondin@protonmail.com>
2023-04-27 22:07:24 +0200

    Added an eventListener and conversationListener
    The eventListener listens for changes in the database and uses
    the conversationAdapter to update the RecyclerView on the MainActivity
    page. The listenConversations function will listen for changes in the database
    for new conversations and when it finds any it will call the eventListener function.

--- app/src/main/java/com/example/chatapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.chatapp.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
@@ -9,21 +10,26 @@ import com.example.chatapp.databinding.ActivityMainBinding
 import com.example.chatapp.utilities.Constants
 import com.example.chatapp.utilities.PreferenceManager
 import android.util.Base64
+import android.view.View
 import android.widget.Toast
 import com.example.chatapp.adapters.RecentConversationsAdapter
 import com.example.chatapp.models.ChatMessage
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.firestore.DocumentChange
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.messaging.FirebaseMessaging
+import java.util.Collections
 
 class MainActivity : AppCompatActivity()
 {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var preferenceManager: PreferenceManager
-    private lateinit var conversations: List<ChatMessage>
+    private lateinit var conversations: MutableList<ChatMessage>
     private lateinit var conversationsAdapter: RecentConversationsAdapter
     private lateinit var database: FirebaseFirestore
 
@@ -37,11 +43,12 @@ class MainActivity : AppCompatActivity()
         loadUserDetails()
         getToken()
         setListeners()
+        listenConversations()
     }
 
     private fun init()
     {
-        conversations = ArrayList()
+        conversations = mutableListOf()
         conversationsAdapter = RecentConversationsAdapter(conversations)
         binding.conversationsRecyclerView.adapter = conversationsAdapter
         database = FirebaseFirestore.getInstance()
@@ -68,6 +75,76 @@ class MainActivity : AppCompatActivity()
     private fun showToast(message: String)
     {
         Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun listenConversations()
+    {
+        database.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+            .whereEqualTo(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+            .addSnapshotListener(eventListener)
+        database.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+            .whereEqualTo(Constants.KEY_RECEIVER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
+            .addSnapshotListener(eventListener)
+    }
+
+    private val eventListener = object : com.google.firebase.firestore.EventListener<QuerySnapshot>
+    {
+        @SuppressLint("NotifyDataSetChanged")
+        override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?)
+        {
+            if (error != null)
+            {
+                return
+            }
+            if (value != null)
+            {
+                for (documentChange in value.documentChanges)
+                {
+                    if (documentChange.type == DocumentChange.Type.ADDED)
+                    {
+                        val chatMessage = ChatMessage()
+                        var senderId: String = documentChange.document.getString(Constants.KEY_SENDER_ID)!!
+                        var receiverId: String = documentChange.document.getString(Constants.KEY_RECEIVER_ID)!!
+                        chatMessage.senderId = senderId
+                        chatMessage.receiverId = receiverId
+                        if(preferenceManager.getString(Constants.KEY_USER_ID).equals(senderId))
+                        {
+                            chatMessage.conversationImage = documentChange.document.getString(Constants.KEY_RECEIVER_IMAGE)!!
+                            chatMessage.conversationName = documentChange.document.getString(Constants.KEY_RECEIVER_NAME)!!
+                            chatMessage.conversationId = documentChange.document.getString(Constants.KEY_RECEIVER_ID)!!
+                        }
+                        else
+                        {
+                            chatMessage.conversationImage = documentChange.document.getString(Constants.KEY_SENDER_IMAGE)!!
+                            chatMessage.conversationName = documentChange.document.getString(Constants.KEY_SENDER_NAME)!!
+                            chatMessage.conversationId = documentChange.document.getString(Constants.KEY_SENDER_ID)!!
+                        }
+                        chatMessage.message = documentChange.document.getString(Constants.KEY_LAST_MESSAGE)!!
+                        chatMessage.dateObject = documentChange.document.getDate(Constants.KEY_TIMESTAMP)!!
+                        conversations.add(chatMessage)
+                    }
+                    else if(documentChange.type == DocumentChange.Type.MODIFIED)
+                    {
+                        for(i in 0 until conversations.size)
+                        {
+                            var senderId: String = documentChange.document.getString(Constants.KEY_SENDER_ID)!!
+                            var receiverID: String = documentChange.document.getString(Constants.KEY_RECEIVER_ID)!!
+                            if(conversations[i].senderId.equals(senderId) && conversations[i].receiverId.equals(receiverID))
+                            {
+                                conversations[i].message = documentChange.document.getString(Constants.KEY_LAST_MESSAGE)!!
+                                conversations[i].dateObject = documentChange.document.getDate(Constants.KEY_TIMESTAMP)!!
+                                break
+                            }
+                        }
+                    }
+                }
+                conversations.sortedWith(compareBy { it.dateObject } )
+                conversationsAdapter.notifyDataSetChanged()
+                binding.conversationsRecyclerView.smoothScrollToPosition(0)
+                binding.conversationsRecyclerView.visibility = View.VISIBLE
+                binding.progressBar.visibility = View.GONE
+            }
+        }
     }
 
     private fun getToken()
